fbb7fc9990dd02c82e20197c98b1574e6fb9e69a
Ajeet D'Souza <98ajeet@gmail.com>
2022-01-25 05:10:33 +0530

    Add integration tests

---
+++ tests/lang.rs
@@ -0,0 +1,36 @@
+use lox::syntax::grammar::ProgramParser;
+use lox::syntax::lexer::Lexer;
+use lox::vm::compiler::Compiler;
+use lox::vm::VM;
+
+use regex::Regex;
+use test_generator::test_resources;
+
+use std::fs;
+use std::io::Write;
+
+thread_local! {
+    static RE_EXPECT: Regex = Regex::new(r" // expect: (.*)").unwrap();
+}
+
+#[test_resources("tests/lang/**/*.lox")]
+fn run_file(path: &str) {
+    let source = fs::read_to_string(path).unwrap();
+    let tokens = Lexer::new(&source);
+    let program = ProgramParser::new().parse(tokens.into_iter()).unwrap();
+    let compiler = Compiler::new_script();
+    let function = compiler.compile(&program).unwrap();
+
+    let mut got = Vec::new();
+    let mut vm = VM::new(&mut got);
+    vm.run(function);
+
+    let mut expected = Vec::new();
+    RE_EXPECT.with(|re| {
+        for captures in re.captures_iter(&source) {
+            writeln!(expected, "{}", &captures[1]).unwrap();
+        }
+    });
+
+    assert_eq!(got, expected);
+}
---
+++ tests/lang/scanning/numbers.lox
@@ -0,0 +1,12 @@
+123
+123.456
+.456
+123.
+
+// expect: NUMBER 123 123.0
+// expect: NUMBER 123.456 123.456
+// expect: DOT . null
+// expect: NUMBER 456 456.0
+// expect: NUMBER 123 123.0
+// expect: DOT . null
+// expect: EOF  null
