070a0e37ba5fbb6182ccb75ca6aa00a13c539822
crazystylus <kartik.sharma522@gmail.com>
2022-04-03 04:51:47 +0530

    Implement using workers

--- crates/lox-wasm/src/lib.rs
+++ crates/lox-wasm/src/lib.rs
@@ -1,33 +1,28 @@
 use std::io::Write;
 
+use lox_syntax::parser::ParserError;
 use lox_vm::vm::VM;
 use wasm_bindgen::prelude::*;
-use web_sys::Element;
+use wasm_bindgen::JsValue;
+use web_sys::MessagePort;
 
-struct Output(Element);
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+struct Output(MessagePort);
 
 impl Output {
-    const ELEMENT_ID: &'static str = "output";
-
-    fn new() -> Self {
-        let output = web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .get_element_by_id(Self::ELEMENT_ID)
-            .unwrap();
-        Self(output)
-    }
-
-    fn clear(&self) {
-        self.0.set_text_content(Some(""));
+    fn new(port: MessagePort) -> Self {
+        Self(port)
     }
 }
 
-impl Write for &Output {
+impl Write for &mut Output {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         let buf = std::str::from_utf8(buf).unwrap();
-        self.0.append_with_str_1(buf).unwrap();
+        self.0.post_message(&JsValue::from_str(buf)).unwrap();
         Ok(buf.len())
     }
 
@@ -37,13 +32,67 @@ impl Write for &Output {
 }
 
 #[wasm_bindgen]
-pub fn lox_run(source: &str) {
-    let output = Output::new();
-    output.clear();
-
-    let program = lox_syntax::parse(source).unwrap();
+pub fn lox_run(source: &str, ws_stdout: MessagePort, ws_stderr: MessagePort) {
+    let stdout = &mut Output::new(ws_stdout);
+    let mut stderr = &mut Output::new(ws_stderr);
+    let program = match lox_syntax::parse(source) {
+        Ok(val) => val,
+        Err(err) => {
+            report_err(source, err, stderr);
+            return;
+        }
+    };
     let compiler = lox_vm::compiler::Compiler::new();
-    let function = compiler.compile(&program).unwrap();
+    let function = match compiler.compile(&program) {
+        Ok(val) => val,
+        Err(err) => {
+            write!(stderr, "{:?}", err).unwrap();
+            return;
+        }
+    };
+    VM::new(stdout, stderr, false).run(function);
+}
 
-    VM::new(&output, &output, false).run(function);
+fn report_err(source: &str, err: ParserError, stderr: &mut Output) {
+    use codespan_reporting::diagnostic::{Diagnostic, Label};
+    use codespan_reporting::files::SimpleFile;
+    use codespan_reporting::term::termcolor;
+    use codespan_reporting::term::{self, Config};
+
+    let (label, range, notes);
+    match err {
+        ParserError::ExtraToken { token } => {
+            label = "unexpected token";
+            range = token.0..token.2;
+            notes = Vec::new();
+        }
+        ParserError::InvalidToken { location } => {
+            label = "invalid token";
+            range = location..location;
+            notes = Vec::new();
+        }
+        ParserError::UnrecognizedEOF { location, expected } => {
+            label = "unrecognized EOF";
+            range = location..location;
+            notes = vec![format!("expected one of: {} after this token", expected.join(", "))];
+        }
+        ParserError::UnrecognizedToken { token, expected } => {
+            label = "unrecognized token";
+            range = token.0..token.2;
+            notes = vec![format!("expected one of: {} after this token", expected.join(", "))];
+        }
+        ParserError::User { error: err } => {
+            label = "unexpected input";
+            range = err.location..err.location + 1;
+            notes = Vec::new();
+        }
+    };
+    let mut writer = termcolor::NoColor::new(stderr);
+    let config = Config::default();
+    let file = SimpleFile::new("src_file", source);
+    let diagnostic = Diagnostic::error()
+        .with_message(label)
+        .with_labels(vec![Label::primary((), range)])
+        .with_notes(notes);
+    term::emit(&mut writer, &config, &file, &diagnostic).unwrap();
 }
