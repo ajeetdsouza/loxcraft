c96e58c2de13ccbcb2e13753b034cbe075221580
Ajeet D'Souza <98ajeet@gmail.com>
2022-05-10 04:30:34 +0530

    It works!

--- crates/lox-playground/ui/src/App.tsx
+++ crates/lox-playground/ui/src/App.tsx
@@ -1,12 +1,190 @@
-import React, { useEffect, useState } from "react";
-import init, { add } from "lox-wasm";
+import React, { useState, useEffect } from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/ext-language_tools";
+import Split from "react-split";
+// @ts-ignore
+import Logo from "./assets/lox.png";
 
-export function App() {
-  const [ans, setAns] = useState(0);
+export function App(): JSX.Element {
+  // Send resize signal to editor on initialization.
+  // https://github.com/securingsincity/react-ace/issues/708
   useEffect(() => {
-    init().then(() => {
-      setAns(add(1, 1));
-    });
+    window.dispatchEvent(new Event("resize"));
   }, []);
-  return <h1>{ans}</h1>;
+
+  // Editor text is saved to local storage.
+  const [editorText, setEditorText] = useState<string>(LocalStorage.editorText);
+  useEffect(() => {
+    LocalStorage.editorText = editorText;
+  }, [editorText]);
+
+  // Output from Lox is continuously streamed here.
+  const [outputText, setOutputText] = useState<string>("");
+  const addOutputText = (text: string) => {
+    setOutputText((outputText) => outputText + text);
+  };
+
+  // The worker is set back to null once it finishes executing.
+  const [worker, setWorker] = useState<Worker>(null);
+  const stopWorker = () => {
+    setWorker((worker) => {
+      if (worker !== null) {
+        worker.terminate();
+      }
+      return null;
+    });
+  };
+
+  const startLox = () => {
+    stopWorker();
+    setOutputText("");
+
+    const worker = new Worker(new URL("worker.ts", import.meta.url), {
+      type: "module",
+    });
+    worker.onmessage = (event) => {
+      const msg: LoxOutMessage = JSON.parse(event.data);
+      switch (msg.type) {
+        case "Output":
+          addOutputText(msg.text);
+          break;
+        case "ExitSuccess":
+          stopWorker();
+          addOutputText("---\nProgram exited successfully.\n");
+          break;
+        case "ExitFailure":
+          stopWorker();
+          addOutputText("---\nProgram exited with an error.\n");
+          break;
+        case "CompileFailure":
+          stopWorker();
+          addOutputText("---\nCompiler exited with an error.\n");
+          break;
+      }
+    };
+    worker.postMessage(editorText);
+    setWorker(worker);
+  };
+
+  const stopLox = () => {
+    stopWorker();
+    addOutputText("---\nCommand terminated.");
+  };
+
+  const isRunning = worker !== null;
+  return (
+    <>
+      <Nav isRunning={isRunning} runClick={isRunning ? stopLox : startLox} />
+      <Split
+        className="d-flex"
+        cursor="col-resize"
+        direction="horizontal"
+        id="content"
+        // Send resize signal to editor on split resize.
+        // https://github.com/securingsincity/react-ace/issues/708
+        onDragEnd={() => window.dispatchEvent(new Event("resize"))}
+      >
+        <Editor text={editorText} onChange={setEditorText} />
+        <Output text={outputText} />
+      </Split>
+    </>
+  );
+}
+
+function Nav(props: NavProps): JSX.Element {
+  let runIcon = "bi-play-fill";
+  let runColor = "btn-success";
+  let runText = "Run";
+  if (props.isRunning) {
+    runIcon = "bi-stop-fill";
+    runColor = "btn-danger";
+    runText = "Stop";
+  }
+
+  return (
+    <nav className="navbar navbar-light border bg-light px-3" id="navbar">
+      <div className="navbar-brand">
+        <img src={Logo} className="me-2" />
+        Lox Playground
+      </div>
+      <a
+        id="run-btn"
+        className={`btn bi ${runIcon} ${runColor}`}
+        onClick={props.runClick}
+      >
+        {runText}
+      </a>
+    </nav>
+  );
+}
+
+function Editor(props: EditorProps): JSX.Element {
+  return (
+    <AceEditor
+      className="h-100 font-monospace fs-6"
+      enableBasicAutocompletion={true}
+      enableLiveAutocompletion={true}
+      focus={true}
+      mode="text"
+      name="editor"
+      onChange={props.onChange}
+      showPrintMargin={false}
+      value={props.text}
+    />
+  );
+}
+
+function Output(props: OutputProps): JSX.Element {
+  return (
+    <pre
+      className="h-100 bg-light font-monospace fs-6"
+      dangerouslySetInnerHTML={{ __html: props.text }}
+      id="output"
+    />
+  );
+}
+
+type NavProps = {
+  isRunning: Boolean;
+  runClick: () => void;
+};
+
+type EditorProps = {
+  text: string;
+  onChange: (text: string) => void;
+};
+
+type OutputProps = {
+  text: string;
+};
+
+type LoxOutMessage =
+  | LoxOutMessageOutput
+  | LoxOutMessageCompileFailure
+  | LoxOutMessageExitFailure
+  | LoxOutMessageExitSuccess;
+type LoxOutMessageOutput = {
+  type: "Output";
+  text: string;
+};
+type LoxOutMessageCompileFailure = {
+  type: "CompileFailure";
+};
+type LoxOutMessageExitFailure = {
+  type: "ExitFailure";
+};
+type LoxOutMessageExitSuccess = {
+  type: "ExitSuccess";
+};
+
+class LocalStorage {
+  static #editorTextKey = "editorText";
+
+  static get editorText(): string {
+    return localStorage.getItem(this.#editorTextKey) || "";
+  }
+
+  static set editorText(text: string) {
+    localStorage.setItem(this.#editorTextKey, text);
+  }
 }
--- crates/lox-playground/ui/src/index.tsx
+++ crates/lox-playground/ui/src/index.tsx
@@ -1,5 +1,6 @@
 import { App } from "./App";
 import { createRoot } from "react-dom/client";
+import "./index.css";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
