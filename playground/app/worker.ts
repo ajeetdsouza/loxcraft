0741f9f9ed0765b378345932a8f909b0c5965f8e
Ajeet D'Souza <98ajeet@gmail.com>
2026-04-10 06:07:48 +0530

    Catch errors when calling web workers

--- playground/app/worker.ts
+++ playground/app/worker.ts
@@ -2,5 +2,11 @@ import init, { loxRun } from "lox-wasm";
 
 onmessage = async (event) => {
   await init();
-  loxRun(event.data as string);
+  try {
+    loxRun(event.data as string);
+  } catch (e) {
+    const text = `RuntimeError: ${e instanceof Error ? e.message : e}\n`;
+    postMessage(JSON.stringify({ type: "Output", text }));
+    postMessage(JSON.stringify({ type: "ExitFailure" }));
+  }
 };
